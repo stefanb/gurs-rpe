7606d178a27f0b83095a8db85ca08a0b0fbf60b0
Štefan Baebler <stefan.baebler@gmail.com>
2024-04-14 21:00:07 +0200

    Get & reproject HS.csv from new CSV source (#8)

--- getSource.sh
+++ getSource.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
+set -e
 DownloadDest="${1}"
 TempDest="${2}"
-credentialsFile="CREDENTIALS-egp.gu.gov.si.txt"
 maxAge=240
-baseUrl="https://egp.gu.gov.si/egp/"
 
 SEDCMD="sed"
 STATCMD="stat"
@@ -21,16 +20,31 @@ MINGW*) machine=MinGw ;;
 esac
 echo Running on: "${machine}", using $SEDCMD and $STATCMD commands
 
+# pass numeric file ids and name as parameter
+function downloadFile() {
+	mkdir -p "${DownloadDest}"
+	echo "Downloading ${DownloadDest}$3..."
+	curl \
+		--compressed \
+		--output "${DownloadDest}$3" \
+		--fail \
+		--progress-bar \
+		"https://ipi.eprostor.gov.si/jgp-service-api/display-views/groups/$1/composite-products/$2/file?filterParam=DRZAVA&filterValue=1"
+}
+
 function extractDownloaded() {
+	rm -rf "${TempDest}" || true
+	mkdir -p "${TempDest}"
+
 	#----- extract: -------
 	for file in "${DownloadDest}"RPE_*.ZIP; do
 		extdir=$(basename "$file" .ZIP)
 		echo "$extdir"
 		unzip -o -d "${TempDest}$extdir" "$file"
 	done
-	for file in "${TempDest}"RPE_*/*.zip; do unzip -o -d "${TempDest}" "$file"; done
+	# for file in "${TempDest}"RPE_*/*.zip; do unzip -o -d "${TempDest}" "$file"; done
 
-	$STATCMD -c '%y' "${TempDest}OB/OB.shp" | cut -d' ' -f1 >"${TempDest}timestamp.txt"
+	# $STATCMD -c '%y' ${TempDest}RPE_HS/KN_SLO_NASLOVI_HS_naslovi_hs_????????.csv | cut -d' ' -f1 >"${TempDest}timestamp.txt"
 }
 
 countTooOld=3
@@ -43,114 +57,13 @@ fi
 # exit if all are newer than max age
 if [ "$countTooOld" -gt "0" ]; then
 	echo "Need to download $countTooOld files (they are either missing or older than $maxAge minutes)"
+	#------ Download all data we care about: ------
+	downloadFile 119 12 RPE_PE.ZIP
+	downloadFile 119 181 RPE_UL.ZIP
+	downloadFile 121 141 RPE_HS.ZIP
 else
 	echo "No need to download anything (source files are already there and not older than $maxAge minutes)"
-	extractDownloaded
-	exit 0
 fi
-
-
-# Clean up leftovers from previous failed runs
-rm -f "${DownloadDest}cookies.txt"
-rm -f "${DownloadDest}login.html"
-
-commonWgetParams=(--load-cookies "${DownloadDest}cookies.txt" --save-cookies "${DownloadDest}cookies.txt" --directory-prefix "${DownloadDest}" --keep-session-cookies --ca-certificate "sigov-ca2.pem")
-# --no-hsts
-# --quiet
-# --ciphers "HIGH:!aNULL:!MD5:!RC4" \
-# --secure-protocol=TLSv1 \	
-# --referer "${baseUrl}" \
-
-function prepareCredentials() {
-	#------ username & password: ------
-	# read possibly existing credentials...
-	# shellcheck source=/dev/null
-	source "$credentialsFile"
-
-	echo Credentials for ${baseUrl}
-
-	if [ -z "$username" ]; then
-		echo -n "	Username: "
-		read -r username
-		echo "username=\"$username\"" >"$credentialsFile"
-	else
-		echo "	Username: '$username'"
-	fi
-
-	if [ -z "$password" ]; then
-		echo -n "	Password: "
-		read -r password
-		read -p "	Save password in plain text to $credentialsFile for future use? (y/N) " -n 1 -r
-		echo # (optional) move to a new line
-		if [[ $REPLY =~ ^[Yy]$ ]]; then
-			# save it only if wanted
-			echo "password=\"$password\"" >>"$credentialsFile"
-		fi
-	else
-		echo "	Password: *********"
-	fi
-}
-
-function login() {
-	#------ Log in to the server.  This can be done only once ------
-	wget "${commonWgetParams[@]}" \
-		--quiet \
-		"${baseUrl}login.html"
-
-	# example login.html content:
-	# <input type="hidden" name="_csrf" value="089070ed-b40a-4e3c-ab22-422de0daffff" />
-	csrftoken="$($SEDCMD -n 's/.*name="_csrf"\s\+value="\([^"]\+\).*/\1/p' "${DownloadDest}login.html")"
-
-	if [ -z "${csrftoken}" ]; then
-		echo "No CSRF token found, exitting!"
-		exit 1
-	fi
-
-	echo "Got CSRF token: \"${csrftoken}\"."
-
-	echo "TRAVIS=${TRAVIS}"
-	if [ "${TRAVIS}" != "true" ]; then
-		prepareCredentials
-	else
-		echo "Running in TRAVIS CI, using encrypted credentials."
-	fi
-
-
-	loginFormData="username=${username}&password=${password}&_csrf=${csrftoken}"
-	#echo login form data: $loginFormData
-
-	#exit 1
-	wget "${commonWgetParams[@]}" \
-		--post-data "${loginFormData}" \
-		--delete-after \
-		--quiet \
-		"${baseUrl}login.html"
-}
-
-
-# pass numeric file id as parameter
-function downloadFile() {
-	wget "${commonWgetParams[@]}" \
-		-q --show-progress \
-		--content-disposition -N \
-		"${baseUrl}download-file.html?id=$1&format=10&d96=1"
-}
-
-# ---------------------------------------------
-login
-
-#------ Download all data we care about: ------
-#RPE_PE.ZIP
-downloadFile 105
-
-#RPE_UL.ZIP
-downloadFile 106
-
-#RPE_HS.ZIP
-downloadFile 107
-
-# Clean up secrets so they are not cached
-rm -f "${DownloadDest}cookies.txt"
 
 
 extractDownloaded
